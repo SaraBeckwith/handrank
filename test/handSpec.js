47091a8b66daf45c58227fdba5dfa2168e829063
Sara Beckwith <sara.beckwith@gmail.com>
2014-03-01 15:39:24 -0800

    Test & Function to check for flush

--- test/handSpec.js
+++ test/handSpec.js
@@ -11,5 +11,23 @@ describe("Hand", function(){
 
 			expect(results).to.deep.equal(expected);
 		});
+
+		//todo: test that it does some minimal input checking
+		//      Each card has rank and suit
+		// 	 	suit is only s,h,d,c (lower)
+
+
+		//todo: have it organize hand by rank order
+
+	});
+
+	describe("#isFlush()", function(){
+		it("should return true when all suits in hand match", function(){
+			var sampleNonFlush = ["Ah", "As", "10c", "7d", "6s"];
+			var sampleFlush = ["Ah", "Kh", "Qh", "Jh", "10h"];
+
+			expect(hand.isFlush(sampleNonFlush)).to.be.false;
+			expect(hand.isFlush(sampleFlush)).to.be.true;
+		});
 	});
 });
