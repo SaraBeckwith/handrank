fcaa5eec1afa85e6b69a97889f0e5bb4284fdf3f
Sara Beckwith <sara.beckwith@gmail.com>
2014-03-01 01:18:39 -0800

    First test

---
+++ test/handSpec.js
@@ -0,0 +1,15 @@
+var expect = require("chai").expect;
+var hand = require("../lib/hand.js");
+
+describe("Hand", function(){
+	describe("#parse()", function(){
+		it("should parse a string into a set of 5 cards, each with rank and suit", function(){
+			var samplehand = "Ah As 10c 7d 6s";
+			var results = hand.parse(samplehand);
+
+			var expected = ["Ah", "As", "10c", "7d", "6s"];
+
+			expect(results).to.equal(expected);
+		});
+	});
+});
